5721aa0224fe06125a20a883f2b6d677fa01a1c6
Kyle Harding <kyle@balena.io>
2021-01-04 18:53:54 -0500

    balena-cli-docker: add buster support
    Change-type: patch
    Signed-off-by: Kyle Harding <kyle@balena.io>

---
+++ README.md
@@ -0,0 +1,11 @@
+# balena-cli-docker
+
+Multiarch docker images with balena-cli and a small footprint.
+
+## Deploy
+
+Set `DOCKER_REPO` and `BALENA_CLI_VERSION` in `deploy.sh` and run.
+
+```bash
+./deploy.sh
+```
--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
-export BALENA_CLI_VERSION=12.37.0
+export DOCKER_REPO="klutchell/balena-cli"
+export BALENA_CLI_VERSION="12.37.0"
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
-docker buildx build . \
+docker buildx build alpine \
     --build-arg BALENA_CLI_VERSION \
-    --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 \
-    --tag "klutchell/balena-cli:${BALENA_CLI_VERSION}-alpine" \
-    --tag "klutchell/balena-cli:latest" \
+    --platform linux/amd64,linux/arm/v7,linux/arm/v6 \
+    --tag "${DOCKER_REPO}:${BALENA_CLI_VERSION}-alpine" \
+    --tag "${DOCKER_REPO}:latest" \
+    --pull --push
+
+docker buildx build buster-slim \
+    --build-arg BALENA_CLI_VERSION \
+    --platform linux/amd64,linux/arm64,linux/arm/v7 \
+    --tag "${DOCKER_REPO}:${BALENA_CLI_VERSION}-buster-slim" \
     --pull --push
