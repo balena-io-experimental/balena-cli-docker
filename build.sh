e4988cbe4dce8d9050da167b096661b90cbdfd02
Kyle Harding <kyle@balena.io>
2021-07-29 10:00:32 -0400

    Separate build and deploy scripts
    Change-type: patch
    Signed-off-by: Kyle Harding <kyle@balena.io>

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+export DOCKER_REPO="${1:-klutchell}/balena-cli"
+export BALENA_CLI_VERSION="${2:-12.38.5}"
+export DOCKER_BUILDKIT=1
+export DOCKER_CLI_EXPERIMENTAL=enabled
+
+docker buildx build . \
+    --pull \
+    --build-arg BALENA_CLI_VERSION \
+    --tag "${DOCKER_REPO}:${BALENA_CLI_VERSION}" \
+    --tag "${DOCKER_REPO}:latest" \
+    --load
